d0a728b2bb6acae9a85513195ec1337715294c23
1082318 <francisco.perezh@elektra.com.mx>
2023-08-25 15:53:01 -0600

    Add env & add main page

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import requests from '../Request'
+import axios from 'axios'
+
+
+
+const Main = () => {
+    const [movies, setMovies] = useState([]) 
+
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [])
+
+    console.log(movies)
+
+  return (
+    <div>Main</div>
+
+
+
+  )
+}
+
+export default Main
