48fdc49307ed9f1b9886fe3bfd423f3269d2d508
1082318 <francisco.perezh@elektra.com.mx>
2023-08-25 17:35:19 -0600

    Add random movie

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,12 @@ function getRandom(number) {
   return Math.floor(Math.random() * number );
 }
 
-export { getRandom };
+const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + '...';
+    } else {
+      return str;
+    }
+  };
+
+export { getRandom, truncateString };
